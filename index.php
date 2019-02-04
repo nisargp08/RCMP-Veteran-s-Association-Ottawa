608056159f59a9d82c8e6458d7cdd79fec81a412
Nisarg Patel <d2mmrclimb@gmail.com>
2019-02-04 17:04:09 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+function test($name,$color){
+echo "<h1>Hello my name is $name and my favourite color is $color</h1>";
+}
+test("Nisarg","Blue");
+?>
+<h2>Name of the website is <?php bloginfo('name')?></h2>
+<h4>The tagline of our website is <?php bloginfo('description') ?> </h4>
