9d209de68f339d3341c8c11f8ec70b6793f8abf1
Nisarg Patel <d2mmrclimb@gmail.com>
2019-02-27 16:25:41 -0500

    Initial Commit

--- archive-events.php
+++ archive-events.php
@@ -20,17 +20,28 @@
     ?>
      <div class="container">
         <div class="content-container">
+            <!-- Looping through custom post types to display all the events -->
+            <?php 
+                $events = new WP_Query(array(
+                    'post_type' => 'Events'
+                ));
+                while($events->have_posts()) { 
+                    $events->the_post(); 
+            ?>
                 <div class="card card-body bg-light">
-                    <div class="row">
-                        <div class="col-md-4 col-sm-4">
-                            <img class="card-img-top" src="<?php get_stylesheet_directory_uri(the_field('event_image'));?>" alt="" style="height:25%;"></img>
-                        </div> 
-                        <div class="col-md-8 col-sm-8">
-                            <h3 class="card-title"><?php the_title(); ?></h3>
-                            <p class="card-text"><?php the_content() ?></p>
+                        <div class="row">
+                            <div class="col-md-4 col-sm-4">
+                                <img class="card-img-top" src="<?php get_stylesheet_directory_uri(the_field('event_image'));?>" alt="" style="height:25%;"></img>
+                            </div> 
+                            <div class="col-md-8 col-sm-8">
+                                <h3 class="card-title"><?php the_title(); ?></h3>
+                                <p class="card-text"><?php the_content() ?></p>
+                            </div>
                         </div>
-                    </div>
-                </div>   
+                    </div> 
+            <!-- WHile loop for displaying events ends -->
+            <?php } ?>
+                
         </div>
     </div>
 
